9052023e71c69c5caeca8089e911fa10de2cd28c
Justin Rhude <justin.rhude52@gmail.com>
2017-01-06 15:36:28 -0500

    :/ php \:

--- contact.php
+++ contact.php
@@ -2,12 +2,15 @@
 
   if($_POST)
   {
-    $name=filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $name=$_POST['name']
+    $email=$_POST['email']
+    $message=$_POST['message']
 
-    $email=filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $message=filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+    //$name=filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    //$email=filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    //$message=filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
-    $concat_message = "Name: " . $name . "\nEmail: " . $email . "\n\n" . $message;
+    $concat_message = "From: " . $name . "\nEmail: " . $email . "\n\n" . $message;
 
     mail("jrhude@justinrhude.com", "Message from " . $name . " on justinrhude.com", $concat_message);
   } else {
